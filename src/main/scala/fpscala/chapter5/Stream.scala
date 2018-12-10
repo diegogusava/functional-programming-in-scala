6f7fa60026ff186511545114bf0dd5519f13674f
Diego Armando Gusava <diegogusava@gmail.com>
2018-12-10 19:42:39 +0100

    Added exercises 5.1, 5.2, 5.3

--- src/main/scala/fpscala/chapter5/Stream.scala
+++ src/main/scala/fpscala/chapter5/Stream.scala
@@ -1,6 +1,27 @@
 package fpscala.chapter5
 
-trait Stream[+A]
+trait Stream[+A] {
+
+  def toList: List[A] = this match {
+    case Empty => List()
+    case Cons(h, t) => h() :: t().toList
+  }
+
+  def take(n: Int): List[A] = this match {
+    case Empty => List()
+    case Cons(h, t) => if (n > 0) h() :: t().take(n - 1) else List()
+  }
+
+  def drop(n: Int): List[A] = this match {
+    case Empty => List()
+    case Cons(h, t) => if (n > 0) t().drop(n - 1) else this.toList
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => Stream.cons(h(), t() takeWhile p)
+    case _ => Stream.empty
+  }
+}
 
 case object Empty extends Stream[Nothing]
 
--- src/test/scala/fpscala/chapter5/StreamTest.scala
+++ src/test/scala/fpscala/chapter5/StreamTest.scala
@@ -4,4 +4,29 @@ import org.scalatest.FunSuite
 
 class StreamTest extends FunSuite {
 
+  test("Exercise 5.1: Write a function to convert a Stream to a List") {
+    val list = Stream(1, 2, 3).toList
+    assert(list == List(1, 2, 3))
+  }
+
+  test("Exercise 5.2: Write the function take(n) for returning the first n elements of a Stream, " +
+    "and drop(n) for skipping the first n elements of a Stream. ") {
+
+    assert(Stream(1, 2, 3).take(0) == List())
+    assert(Stream(1, 2, 3).take(1) == List(1))
+    assert(Stream(1, 2, 3).take(3) == List(1, 2, 3))
+
+    assert(Stream(1, 2, 3).drop(0) == List(1, 2, 3))
+    assert(Stream(1, 2, 3).drop(1) == List(2,3))
+    assert(Stream(1, 2, 3).drop(3) == List())
+    assert(Stream(1, 2, 3).drop(4) == List())
+  }
+
+  test("Exercise 5.3: Write the function takeWhile for returning all starting elements of a Stream that " +
+    "match the given predicate. ") {
+    assert(Stream(1, 2, 3).takeWhile(x => x > 0).toList == List(1, 2, 3))
+    assert(Stream(1, 2, 3).takeWhile(x => x < 2).toList == List(1))
+    assert(Stream(1, 2, 3).takeWhile(_ > 4).toList == List())
+  }
+
 }
